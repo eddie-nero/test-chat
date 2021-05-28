d06d91042b3bc6d7a2e1cb7e0bec66a51f9648f6
eddienero <eddienero@yandex.ru>
2021-05-28 14:19:16 +0300

    maybe thats all?

---
+++ scripts/wait-for-postgres.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+### WAITING POSTGRES START ###
+RETRIES=7
+while [ "$RETRIES" -gt 0 ]
+do
+  echo "Waiting for postgres server, $((RETRIES--)) remaining attempts..."
+  PG_STATUS="$(pg_isready -h $host -U postgres)"
+  PG_EXIT=$(echo $?)
+  echo "Postgres Status: $PG_EXIT - $PG_STATUS"
+  if [ "$PG_EXIT" = "0" ];
+    then
+      RETRIES=0
+  fi
+  sleep 5  # timeout for new loop
+done
+
+### DJANGO MANAGE COMMANDS ###
+DEPLOY_FLAG=./deploy_state.flag
+
+touch $DEPLOY_FLAG
+
+python manage.py migrate
+python manage.py collectstatic --no-input
+
+rm -f $DEPLOY_FLAG
+echo "Run Django"
+
+python manage.py runserver 0.0.0.0:8000
